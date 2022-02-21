d11a758c0f78ff38f3eb42d0af69f894508270c0
charmed-quark <martenssoncs@gmail.com>
2022-02-21 10:48:19 +0100

    feat: Increase coverage for BinaryTree.remove() (#2)
    * Adds a test class for the class BinaryTree
    * Includes 7 unit tests that increase the branch coverage of remove() to 61%

--- src/test/java/com/thealgorithms/datastructures/trees/BinaryTreeTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/BinaryTreeTest.java
@@ -10,25 +10,63 @@ class BinaryTreeTest {
 
     public BinaryTree tree;
 
+    /**
+     * Constructs the following tree:
+     *          5
+     *        /   \
+     *       3     7
+     *      / \   / \
+     *     1   4 6   8
+     *    / \         \
+     *   0   2         9
+     */
     @BeforeEach
     public void initBinTree() {
         tree = new BinaryTree();
-        tree.put(5);
-        tree.put(3);
-        tree.put(7);
-        tree.put(2);
-        tree.put(4);
-        tree.put(0);
-        tree.put(1);
-        tree.put(6);
-        tree.put(8);
-        tree.put(9);
-        //tree.inOrder(tree.getRoot());
+        int[] elementsInTree = { 5, 3, 1, 0, 2, 4, 7, 6, 8, 9 };
+        for(int elem : elementsInTree) {
+            tree.put(elem);
+        }
     }
 
     @Test
-    public void testDummy() {
-        assertTrue(true);
+    public void removeReturnsFalseWhenCalledOnNodeThatDoesNotExist() {
+        assertFalse(tree.remove(11));
+    }
+
+    @Test
+    public void removeReturnsTrueWhenCalledOnNodeInTree() {
+        assertTrue(tree.remove(3));
+    }
+
+    @Test
+    public void removingLeftLeafNodeSetsParentsLeftChildToNull() {
+        tree.remove(0);
+        assertEquals(null, tree.find(1).left);
+    }
+
+    @Test
+    public void removingRightLeafNodeSetsParentsRightChildToNull() {
+        tree.remove(9);
+        assertEquals(null, tree.find(8).right);
+    }
+
+    @Test
+    public void removingRootSetsRootToSuccessor() {
+        tree.remove(5);
+        assertEquals(6, tree.getRoot().data);
+    }
+
+    @Test
+    public void singleChildNodeReplacesParentIfParentIsRemoved() {
+        tree.remove(8);
+        assertEquals(9, tree.find(7).right.data);
+    }
+
+    @Test
+    public void findReturnsParentWhenCalledOnRemovedNode() {
+        tree.remove(6);
+        assertEquals(7, tree.find(6).data);
     }
 
     @AfterAll
