f0dc2b945cc4c95aa9759e03015d52e6138a6ddc
Samuel <ssoderbe@kth.se>
2022-02-21 20:31:47 +0100

    #7 fix: Added coverage for the main part of go

--- src/main/java/com/thealgorithms/datastructures/graphs/BellmanFord.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/BellmanFord.java
@@ -7,7 +7,7 @@ class BellmanFord /*Implementation of Bellman ford to detect negative cycles. Gr
 start vertex, end vertex and weights. Vertices should be labelled with a number between 0 and total number of vertices-1,both inclusive*/ {
 
     // For manual code coverage checks
-    public int[] coverage = new int[13];
+    public int[] coverage = new int[14];
 
     int vertex, edge;
     private Edge edges[];
@@ -58,6 +58,7 @@ start vertex, end vertex and weights. Vertices should be labelled with a number 
             go(InputStream input) // Interactive run for understanding the class first time. Assumes source vertex is 0 and
     // shows distance to all vertices
     {
+        coverage[0] += 1;
         Scanner sc = new Scanner(input); // Grab scanner object for user input
         int i, v, e, u, ve, w, j, neg = 0;
         System.out.println("Enter no. of vertices and edges please");
@@ -66,7 +67,7 @@ start vertex, end vertex and weights. Vertices should be labelled with a number 
         Edge arr[] = new Edge[e]; // Array of edges
         System.out.println("Input edges");
         for (i = 0; i < e; i++) {
-            coverage[0] += 1;
+            coverage[1] += 1;
             u = sc.nextInt();
             ve = sc.nextInt();
             w = sc.nextInt();
@@ -77,54 +78,54 @@ start vertex, end vertex and weights. Vertices should be labelled with a number 
         // and all vertices
         int p[] = new int[v]; // Parent array for holding the paths
         for (i = 0; i < v; i++) {
-            coverage[1] += 1;
+            coverage[2] += 1;
             dist[i] = Integer.MAX_VALUE; // Initializing distance values
         }
         dist[0] = 0;
         p[0] = -1;
         for (i = 0; i < v - 1; i++) {
-            coverage[2] += 1;
+            coverage[3] += 1;
             for (j = 0; j < e; j++) {
-                coverage[3] += 1;
+                coverage[4] += 1;
                 if ((int) dist[arr[j].u] != Integer.MAX_VALUE
                         && dist[arr[j].v] > dist[arr[j].u] + arr[j].w) {
-                    coverage[4] += 1;
+                    coverage[5] += 1;
                     dist[arr[j].v] = dist[arr[j].u] + arr[j].w; // Update
                     p[arr[j].v] = arr[j].u;
                 }   else    {
-                    coverage[5] += 1;
+                    coverage[6] += 1;
                 }
             }
         }
         // Final cycle for negative checking
         for (j = 0; j < e; j++) {
-            coverage[6] += 1;
+            coverage[7] += 1;
             if ((int) dist[arr[j].u] != Integer.MAX_VALUE && dist[arr[j].v] > dist[arr[j].u] + arr[j].w) {
-                coverage[7] += 1;
+                coverage[8] += 1;
                 neg = 1;
                 System.out.println("Negative cycle");
                 break;
             }   else    {
-                coverage[8] += 1;
+                coverage[9] += 1;
             }
         }
         if (neg == 0) // Go ahead and show results of computation
         {
-            coverage[9] += 1;
+            coverage[10] += 1;
             System.out.println("Distances are: ");
             for (i = 0; i < v; i++) {
-                coverage[10] += 1;
+                coverage[11] += 1;
                 System.out.println(i + " " + dist[i]);
             }
             System.out.println("Path followed:");
             for (i = 0; i < v; i++) {
-                coverage[11] += 1;
+                coverage[12] += 1;
                 System.out.print("0 ");
                 printPath(p, i);
                 System.out.println();
             }
         }   else    {
-            coverage[12] += 1;
+            coverage[13] += 1;
             return null;
         }
         sc.close();
